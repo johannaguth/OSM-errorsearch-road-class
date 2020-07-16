a061875931b0b0ccd336a369e79ede330fcdd9d0
Johanna Guth <34277335+johannaguth@users.noreply.github.com>
2020-07-16 17:10:52 +0200

    Add files via upload

---
+++ discComp_search/discComp_alllevels.sql
@@ -0,0 +1,19 @@
+-- L1
+SELECT find_subgraphs('ways_nsw_l1'::text);
+SELECT collect_subgraphs('ways_nsw_l1'::text);
+
+-- L2
+SELECT find_subgraphs('ways_bm_l2'::text);
+SELECT collect_subgraphs('ways_nsw_l2'::text);
+
+-- L3
+SELECT find_subgraphs('ways_bm_l3'::text);
+SELECT collect_subgraphs('ways_nsw_l3'::text);
+
+-- L4
+SELECT find_subgraphs('ways_bm_l4'::text);
+SELECT collect_subgraphs('ways_nsw_l4'::text);
+
+-- L5
+SELECT find_subgraphs('ways_bm_l5'::text);
+SELECT collect_subgraphs('ways_nsw_l5'::text);
